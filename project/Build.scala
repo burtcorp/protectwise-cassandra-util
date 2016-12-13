614846a1da5e77b0df3fe5e7c28860ec73c7b7cb
Gustav Munkby <grddev@gmail.com>
2016-12-13 11:50:22 +0100

    Downgrade Cassandra to 2.1.8
    To encourage compatibility with legacy clusters

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object CassandraUtilBuild extends Build {
   val slf4jVersion = "1.7.+"
   val guavaVersion = "17.0"
   val playVersion = "2.3.+"
-  val cassandraVersion = "2.1.14"
+  val cassandraVersion = "2.1.8"
 
   val scalaVersionsToCompile = Seq("2.10.5", "2.11.6")
 
